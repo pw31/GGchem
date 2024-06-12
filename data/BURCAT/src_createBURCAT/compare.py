53e2ad65e7f695c66332a2a89eb6b4bca56cf5df
pw31 <pw31@st-and.ac.uk>
2024-06-12 12:16:18 +0200

    fighting some naming conflicts (HNCO)

--- data/BURCAT/src_createBURCAT/compare.py
+++ data/BURCAT/src_createBURCAT/compare.py
@@ -27,7 +27,7 @@ bk  = 1.38065812E-16    # Boltzman constant erg/K
 
 # set fit and plot ranges
 Tref = 298.15     # reference temperature
-Tmin = 50         # plot range
+Tmin = 20         # plot range
 Tmax = 3500
 N    = 200
 TT   = np.zeros(N,dtype=float)
@@ -112,6 +112,29 @@ for iline in range(0,999999):
       BUfit.append(fit)
 BUnamU = np.array(BUnamU)
 
+#==========================================================================
+# read UMIST Heat_of_formation.dat
+#==========================================================================
+print
+print "reading Heat_of_formation.dat ..."
+f = open('Heat_of_formation.dat','r')
+dum = f.readline()
+Ndat = int(f.readline())
+lines = f.readlines()
+f.close
+UMnam = []
+UMdH0 = []  # standard heat of formation at 0K [kJ/mol]
+for i in range(Ndat):
+  line = lines[i]
+  #print line
+  dat = line.split()[1]
+  if ('..' not in dat):
+    UMnam.append(line.split()[0].upper())
+    UMdH0.append(float(dat))
+UMnam = np.array(UMnam)
+UMdH0 = np.array(UMdH0)
+    
+print "-------------------------------"    
 iplot = 0
 for mol in allnam:
   ind = np.where(GGnam==mol)[0]
@@ -119,38 +142,57 @@ for mol in allnam:
   mol1 = GGnam[i]
   fit1 = GGfit[i]
   print mol1
+  dG_GG = 0.0*TT
+  dG_BU = 0.0*TT
+  a0 = fit1[0]
+  a1 = fit1[1]
+  a2 = fit1[2]
+  a3 = fit1[3]
+  a4 = fit1[4]
+  a5 = fit1[5]
+  a6 = fit1[6]
+  b0 = fit1[7]
+  b1 = fit1[8]
+  b2 = fit1[9]
+  b3 = fit1[10]
+  b4 = fit1[11]
+  b5 = fit1[12]
+  b6 = fit1[13]
+  i = 0
+  for T in TT:
+    if (T>1000.0):
+      dGRT = a0*(1-np.log(T)) -a1*T/2 -a2*T**2/6 -a3*T**3/12 - a4*T**4/20 \
+           + a5/T - a6  
+    else:
+      dGRT = b0*(1-np.log(T)) -b1*T/2 -b2*T**2/6 -b3*T**3/12 - b4*T**4/20 \
+           + b5/T - b6  
+    dG_GG[i] = dGRT*R*T
+    i += 1
+  plt.figure(figsize=(7,8))
+  gs = gridspec.GridSpec(2, 1, height_ratios=[1.8,1])
+  plt.subplot(gs[0])
+  plt.plot(TT,dG_GG/1000,c='orange',lw=4.5,label='GGchem')
+  #plt.xscale('log') 
+  #plt.xscale('log')
+  plt.subplots_adjust(left=0.15,right=0.98,bottom=0.07,top=0.95,hspace=0.09)
+  plt.xlim(-70,1.02*Tmax)
+  #plt.xlabel(r'$T\,\mathrm{[K]}$',fontsize=16)
+  plt.ylabel(r'$\Delta_\mathrm{f} G^0\ \mathrm{[kJ/mol]}$',fontsize=16)
+  plt.title(mol)
+
+  ind = np.where(UMnam==mol)[0]
+  if (len(ind)>0):
+    i = ind[0]
+    dHf0 = UMdH0[i]
+    print "->",i,UMnam[i]
+    plt.scatter(0.0,dHf0,s=150,c='black',marker='*',label='UMIST',zorder=10)
+
   ind = np.where(BUnamU==mol)[0]
   if (len(ind)>0):
     i = ind[0]
     mol2 = BUnam[i]
     fit2 = BUfit[i]
-    print "-->",i,mol2
-    dG_GG = 0.0*TT
-    dG_BU = 0.0*TT
-    a0 = fit1[0]
-    a1 = fit1[1]
-    a2 = fit1[2]
-    a3 = fit1[3]
-    a4 = fit1[4]
-    a5 = fit1[5]
-    a6 = fit1[6]
-    b0 = fit1[7]
-    b1 = fit1[8]
-    b2 = fit1[9]
-    b3 = fit1[10]
-    b4 = fit1[11]
-    b5 = fit1[12]
-    b6 = fit1[13]
-    i = 0
-    for T in TT:
-      if (T>1000.0):
-        dGRT = a0*(1-np.log(T)) -a1*T/2 -a2*T**2/6 -a3*T**3/12 - a4*T**4/20 \
-             + a5/T - a6  
-      else:
-        dGRT = b0*(1-np.log(T)) -b1*T/2 -b2*T**2/6 -b3*T**3/12 - b4*T**4/20 \
-             + b5/T - b6  
-      dG_GG[i] = dGRT*R*T
-      i += 1
+    print "-->",i,mol2    
     a0 = fit2[0]
     a1 = fit2[1]
     a2 = fit2[2]
@@ -176,17 +218,7 @@ for mol in allnam:
       dG_BU[i] = dGRT*R*T
       i+=1
     iplot+=1  
-    plt.figure(figsize=(7,8))
-    gs = gridspec.GridSpec(2, 1, height_ratios=[1.8,1])
-    plt.subplot(gs[0])
-    plt.plot(TT,dG_GG/1000,c='orange',lw=4.5,label='GGchem')
     plt.plot(TT,dG_BU/1000,c='dodgerblue',lw=2.0,ls='--',label='BURCAT')
-    #plt.xscale('log') 
-    #plt.xscale('log')
-    plt.xlim(0,1.02*Tmax)
-    #plt.xlabel(r'$T\,\mathrm{[K]}$',fontsize=16)
-    plt.ylabel(r'$\Delta_\mathrm{f} G^0\ \mathrm{[kJ/mol]}$',fontsize=16)
-    plt.title(mol2)
     plt.legend(loc='best')
     plt.subplot(gs[1])
     plt.plot(TT,(dG_GG-dG_BU)/1000,c='black',lw=2,label='GGchem-BURCAT')
@@ -198,12 +230,11 @@ for mol in allnam:
     plt.ylim(ymin,ymax)
     plt.xlabel(r'$T\,\mathrm{[K]}$',fontsize=16)
     plt.ylabel(r'$\Delta_\mathrm{f} G^0\ \mathrm{[kJ/mol]}$',fontsize=16)
-    plt.subplots_adjust(left=0.15,right=0.98,bottom=0.07,top=0.95,hspace=0.09)
-    plt.legend(loc='best')
-    plt.savefig(pp,format='pdf')
-    plt.clf()
+  plt.legend(loc='best')
+  plt.savefig(pp,format='pdf')
+  plt.clf()
 
-  #if (mol1=='HCO+'):
+  #if (mol1=='C+'):
   #  pp.close()
   #  stop
 
